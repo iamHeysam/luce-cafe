092d59caaf293de6a50b1065fba7aff222e83f7d
iamHeysam <soheilzarei7237@gmail.com>
2025-11-23 20:43:22 +0330

    kdsj

--- js/app.js
+++ js/app.js
@@ -64,19 +64,6 @@ const categories = [
             </svg>`,
     status: "notActive",
   },
-  {
-    category: "dog",
-    text: "سک",
-    svg: ` <svg
-            class="category__icon"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1024 1024">
-            <path
-              d="M768 64a192 192 0 1 1-69.952 370.88L480 725.376V896h96a32 32 0 1 1 0 64H320a32 32 0 1 1 0-64h96V725.376L76.8 273.536a64 64 0 0 1-12.8-38.4v-10.688a32 32 0 0 1 32-32h71.808l-65.536-83.84a32 32 0 0 1 50.432-39.424l96.256 123.264h337.728A192.064 192.064 0 0 1 768 64zM656.896 192.448H800a32 32 0 0 1 32 32v10.624a64 64 0 0 1-12.8 38.4l-80.448 107.2a128 128 0 1 0-81.92-188.16v-.064zm-357.888 64 129.472 165.76a32 32 0 0 1-50.432 39.36l-160.256-205.12H144l304 404.928 304-404.928H299.008z"
-            />
-            </svg>`,
-    status: "notActive",
-  },
 ];
 
 const menu = {
@@ -147,22 +134,23 @@ const menu = {
 
 const swiperContainer = document.querySelector(".mySwiper");
 const menuElem = document.querySelector(".menu");
+const categoriesElem = document.querySelector(".categories");
 
 const showCategories = () => {
-  swiperContainer.innerHTML = "";
+  categoriesElem.innerHTML = "";
 
   categories.forEach((category) => {
-    swiperContainer.insertAdjacentHTML(
+    categoriesElem.insertAdjacentHTML(
       "beforeend",
       `
-        <swiper-slide>
-            <div class="category ${
-              category.status === "active" ? "active" : ""
-            }"  onclick="chooseCategory('${category.category}')">
-              ${category.svg}
-              <span class="category__title">${category.text}</span>
-            </div>
-        </swiper-slide>
+        <div class="category ${category.status === "active" ? "active" : ""}">
+          <div class="category__icon-box" onclick="chooseCategory('${
+            category.category
+          }')">
+            ${category.svg}
+          </div>
+          <span class="category__title">${category.text}</span>
+        </div>
       `
     );
   });
