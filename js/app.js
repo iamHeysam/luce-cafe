45c00d759a83a934af11b07b79791609218ef383
iamHeysam <soheilzarei7237@gmail.com>
2025-12-10 20:36:00 +0330

    dddddddd

--- js/app.js
+++ js/app.js
@@ -134,35 +134,29 @@ const menu = {
 
 const menuElem = document.querySelector(".menu");
 const categoriesElem = document.querySelector(".categories");
+let lastScrollY = window.scrollY;
 
 const showCategoties = () => {
   categoriesElem.innerHTML = "";
-
   let categoryExample = "";
 
   categories.forEach((category) => {
     categoryExample = category.category;
 
-    console.log(menu[categoryExample][1].name);
-
     categoriesElem.insertAdjacentHTML(
       "beforeend",
       `
-      <div class="category">
+      <div class="category ${
+        category.isActive ? "active" : ""
+      }" data-category="${category.category}">
         <div class="category__right">
           <span class="category__title">${category.text}</span>
-          <span class="category__example">${menu[categoryExample][0].name}، ${menu[categoryExample][1].name} و ...</span>
+          <span class="category__example">${menu[categoryExample][0]?.name}، ${
+        menu[categoryExample][1]?.name
+      } و ...</span>
         </div>
         <div class="category__left">
-          <svg
-            class="category__icon"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1024 1024"
-          >
-            <path
-              d="M768 64a192 192 0 1 1-69.952 370.88L480 725.376V896h96a32 32 0 1 1 0 64H320a32 32 0 1 1 0-64h96V725.376L76.8 273.536a64 64 0 0 1-12.8-38.4v-10.688a32 32 0 0 1 32-32h71.808l-65.536-83.84a32 32 0 0 1 50.432-39.424l96.256 123.264h337.728A192.064 192.064 0 0 1 768 64zM656.896 192.448H800a32 32 0 0 1 32 32v10.624a64 64 0 0 1-12.8 38.4l-80.448 107.2a128 128 0 1 0-81.92-188.16v-.064zm-357.888 64 129.472 165.76a32 32 0 0 1-50.432 39.36l-160.256-205.12H144l304 404.928 304-404.928H299.008z"
-            />
-          </svg>
+          ${category.svg}
         </div>
       </div>
       `
@@ -170,7 +164,46 @@ const showCategoties = () => {
   });
 };
 
-let lastScrollY = window.scrollY;
+const chooseMenuCategory = (event) => {
+  const selectedCategory = event.target.closest(".category");
+
+  if (selectedCategory) {
+    document.querySelector(".active").classList.remove("active");
+    selectedCategory.classList.add("active");
+  }
+
+  showMenu(selectedCategory.dataset.category);
+};
+
+const showMenu = (userSelected) => {
+  const categorySelected = userSelected;
+  let menuArray = "";
+
+  if (menu[categorySelected]) {
+    menuArray = menu[categorySelected];
+  } else {
+    menuArray = menu.lunch;
+  }
+
+  menuElem.innerHTML = "";
+
+  menuArray.forEach((categorySelected) => {
+    console.log(categorySelected.name);
+
+    menuElem.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="menu-item">
+        <span class="menu-item__name">${categorySelected.name}</span>
+        <p class="menu-item__about">${categorySelected.about}</p>
+        <span class="menu-item__price">${categorySelected.price}</span>
+      </div>
+      `
+    );
+  });
+};
+
+categoriesElem.addEventListener("click", chooseMenuCategory);
 
 window.addEventListener("scroll", () => {
   const currentY = window.scrollY;
@@ -186,4 +219,5 @@ window.addEventListener("scroll", () => {
 
 window.addEventListener("load", () => {
   showCategoties();
+  showMenu();
 });
